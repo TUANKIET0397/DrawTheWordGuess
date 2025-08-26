60aaa02c39c70ae6bcd33d7cceb2700612977119
TUANKIET0397 <kiet8103@gmail.com>
2025-08-26 15:09:20 +0700

    khởi tạo dự án, cập nhật library express, nodemon, morgan, sass

---
+++ assets/index.js
@@ -0,0 +1,68 @@
+const path = require("path")
+const express = require("express")
+const morgan = require("morgan")
+const { engine } = require("express-handlebars")
+const http = require("http")
+const { Server } = require("socket.io")
+
+const app = express()
+const port = 1607
+
+const route = require("./routes")
+
+// middleware
+app.use(express.urlencoded({ extended: true })) // xử lý dữ liệu từ form
+app.use(express.json()) // xử lý dữ liệu json
+
+// xử lý dạng file tĩnh
+app.use(express.static(path.join(__dirname, "public")))
+app.use("/js", express.static(path.join(__dirname, "js")))
+app.use("/img", express.static(path.join(__dirname, "img")))
+
+// HTTP logger
+app.use(morgan("combined"))
+
+// template engine
+app.engine(
+    ".hbs",
+    engine({
+        extname: ".hbs",
+        allowProtoPropertiesByDefault: true,
+        helpers: {
+            block: function (name) {
+                this._blocks = this._blocks || {}
+                const val = (this._blocks[name] || []).join("\n")
+                return val
+            },
+            contentFor: function (name, options) {
+                this._blocks = this._blocks || {}
+                this._blocks[name] = this._blocks[name] || []
+                this._blocks[name].push(options.fn(this))
+            },
+        },
+    })
+)
+
+app.set("view engine", ".hbs")
+// set views directory - render xong nhảy vào đây tìm
+app.set("views", path.join(__dirname, "resources", "views"))
+
+//IO - Tạo HTTP server từ Express app
+const server = http.createServer(app)
+const io = new Server(server)
+
+// Import socket handlers
+const initializeSocket = require("./sockets")
+initializeSocket(io)
+
+// nạp route vào app
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+const drawRoute = require("./routes/site")
+app.use("/", drawRoute)
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
